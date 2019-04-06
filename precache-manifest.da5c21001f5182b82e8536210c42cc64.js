46d2b86e0f1aa7aed854310da4f4ef855057cd58
maggievu <maggievu91@gmail.com>
2019-04-06 10:41:21 -0700

    Updates

---
+++ precache-manifest.da5c21001f5182b82e8536210c42cc64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a6b021b2c16ab39e43",
+    "url": "/restaurant-react-app/static/js/runtime~main.678a2e08.js"
+  },
+  {
+    "revision": "d85bf4ffe43e59cf643f",
+    "url": "/restaurant-react-app/static/js/main.fc19bc52.chunk.js"
+  },
+  {
+    "revision": "6dd388828ec49ca28dc9",
+    "url": "/restaurant-react-app/static/js/2.efe01d12.chunk.js"
+  },
+  {
+    "revision": "d85bf4ffe43e59cf643f",
+    "url": "/restaurant-react-app/static/css/main.8d42493d.chunk.css"
+  },
+  {
+    "revision": "66512a484e739bde05c9677fad703f8e",
+    "url": "/restaurant-react-app/index.html"
+  }
+];
